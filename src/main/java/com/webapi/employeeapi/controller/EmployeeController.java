d77ba5cc02cf48dcd000ee0dee7523b9c9435c5e
Dauth Gurung <dauth22grg22@gmail.com>
2025-11-08 03:25:22 +1100

    Add EmployeeController with CRUD REST endpoints

---
+++ src/main/java/com/webapi/employeeapi/controller/EmployeeController.java
@@ -0,0 +1,52 @@
+package com.webapi.employeeapi.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.webapi.employeeapi.dto.EmployeeDto;
+import com.webapi.employeeapi.service.EmployeeService;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequestMapping("/api/employee")
+@RequiredArgsConstructor
+public class EmployeeController {
+	
+	private final EmployeeService employeeService;
+	
+	@GetMapping
+	public ResponseEntity<List<EmployeeDto>> getAll(){
+		return ResponseEntity.ok(employeeService.getAllEmployees());
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<EmployeeDto> getById(@PathVariable Long id){
+		return ResponseEntity.ok(employeeService.getEmployeeById(id));
+	}
+	
+	@PostMapping
+	public ResponseEntity<EmployeeDto> create(@RequestBody EmployeeDto dto){
+		return ResponseEntity.ok(employeeService.createEmployee(dto));
+	}
+	
+	@PutMapping("/{id}")
+	public ResponseEntity<EmployeeDto> update(@PathVariable Long id, @RequestBody EmployeeDto dto){
+		return ResponseEntity.ok(employeeService.updateEmployee(id, dto));
+	}
+	
+	@DeleteMapping("/{id}")
+	public ResponseEntity<Void> delete(@PathVariable Long id){
+		employeeService.deleteEmployee(id);
+		return ResponseEntity.noContent().build();
+	}
+}
