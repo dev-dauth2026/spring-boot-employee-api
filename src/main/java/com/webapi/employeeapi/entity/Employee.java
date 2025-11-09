65b6c5b30d85d706161aee46253437856476d8e1
Dauth Gurung <dauth22grg22@gmail.com>
2025-11-09 13:58:15 +1100

    feat(model): enforce unique email and non-null columns; rename table to employees

--- src/main/java/com/webapi/employeeapi/entity/Employee.java
+++ src/main/java/com/webapi/employeeapi/entity/Employee.java
@@ -1,9 +1,11 @@
 package com.webapi.employeeapi.entity;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Index;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -12,7 +14,9 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
-@Table(name = "Employee")
+@Table(name = "Employees", indexes = {
+		@Index(name="uq_employee_email", columnList="email", unique = true)
+})
 @Getter
 @Setter
 @NoArgsConstructor
@@ -24,8 +28,13 @@ public class Employee {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	
+	@Column(nullable = false)
 	private String name;
+	
+	@Column(nullable = false, unique = true)
 	private String email;
+	
+	@Column(nullable = false)
 	private String department;
 
 }
